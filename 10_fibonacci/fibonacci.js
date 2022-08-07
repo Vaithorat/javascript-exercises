3fb15121a479cfcc37e00ff17b7344b82eddfd7e
vaithorat <vaibhavthorat141997@gmail.com>
2022-08-07 18:41:30 +0530

    Solved Oldest

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(number) {
+  if (number < 0) return "OOPS"
+  if (number === 0 ) return 1
+  let a = 0
+  let b = 1
+  for (i=1; i<number ; i++){
+    const temp = b;
+    b = a+b
+    a = temp;
+  }
+  return b;
+  };
 
 // Do not edit below this line
 module.exports = fibonacci;
